89afe74b136aee31ccb26b63bdb9d158c6e95aea
learrsan <gaslas@hotmail.com>
2015-04-06 18:50:38 +0000

    06/04/2015 18:50

---
+++ Usuarios/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="3aabcf4c-355a-4679-a3f0-9d1707172f2b" featureId="3aabcf4c-355a-4679-a3f0-9d1707172f2b" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="Usuarios Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="69535743-334d-43c2-aac2-06b880367c6b" />
+  </projectItems>
+</feature>
---
+++ Usuarios/WebPartUsuarios/WebPartUsuarios.ascx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint;
+
+namespace Usuarios.WebPartUsuarios
+{
+    [ToolboxItemAttribute(false)]
+    public partial class WebPartUsuarios : WebPart
+    {
+        // Uncomment the following SecurityPermission attribute only when doing Performance Profiling on a farm solution
+        // using the Instrumentation method, and then remove the SecurityPermission attribute when the code is ready
+        // for production. Because the SecurityPermission attribute bypasses the security check for callers of
+        // your constructor, it's not recommended for production purposes.
+        // [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Assert, UnmanagedCode = true)]
+        public WebPartUsuarios()
+        {
+        }
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+            var usuario = SPContext.Current.Web.CurrentUser;
+            var roles = SPContext.Current.Web.RoleDefinitions;
+            var texto = String.Format("Hola {0} {1} bienvenido", usuario.Name, usuario.Email);
+
+            lblUsuario.Text = texto;
+
+            var usuarios = SPContext.Current.Web.AllUsers;
+
+            foreach (SPUser usu in usuarios)
+            {
+                var tx = String.Format("Usuario {0} <br/>", usu.Name);
+                lblUsuarios.Text += tx;
+            }
+            
+            foreach (SPRoleDefinition rol in roles)
+            {
+                var tx = String.Format("Rol {0} <br/>", rol.Name);
+                lblRoles.Text += tx;
+            }
+        }
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            InitializeControl();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnOutPerm_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
